82b975983ac75d4ffabed1207136fab53f47bcc3
mrinal1000 <mrinalgrg19@gmail.com>
2024-08-09 09:10:13 +0545

    first

---
+++ function.py
@@ -0,0 +1,44 @@
+# # def value(a,b):
+# #     return a+b
+# # print(value(22,12)
+# def square(x):
+#     return x*x
+# num=[1,2,3,4]
+# final=map(square,num)
+# print(list(final))
+# def is_even(x):
+#     return x%2==0
+# numbers=[1,2,3,4,5]
+# final=filter(is_even,numbers)
+# print(list(final))
+
+# def square(x):
+#     return x*x
+# num=[1,2,3,4,5]
+# is_square=map(square,num)
+# print(list(is_square)
+
+# def is_even(x):
+#     return x%2==0
+# numbers=[1,4,9,16,25]
+# final=filter(is_even,numbers)
+# print(list(final))
+
+# from functools import reduce
+# def sum_num(x,y):
+#     return x+y
+# numbers=[1,4,9,16.25]
+# final=reduce(sum_num,numbers)
+# print(final)
+def cal_percentage(**kwargs):
+    sum=0
+    print    
+
+
+
+
+
+
+
+ 
+
---
+++ x=10.py
@@ -0,0 +1,18 @@
+# def abc(a,b):
+#     return a*b
+# xyz=abc(5,4)
+# print(xyz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
