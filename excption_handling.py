7cb6c9db093ef955f038a5332e048813723a7e6e
mrinal1000 <mrinalgrg19@gmail.com>
2024-08-12 09:28:39 +0545

    exption

---
+++ excption_handling.py
@@ -0,0 +1,12 @@
+# class ashok(Exception):
+#     pass
+# try:
+#     a=[1,2,3,4]
+#     user=int(input("enter a index:"))
+#     if user > len(a):
+#         raise ashok()
+#     else:
+#         print("the index value is:",a[user])        
+# except ashok:
+#     print("this is index error")
+    
