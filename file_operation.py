7321645db40bbe5370fcf053d05f2a671144b57c
mrinal1000 <mrinalgrg19@gmail.com>
2024-08-14 10:43:58 +0545

    file

---
+++ file_operation.py
@@ -0,0 +1,16 @@
+#plain text file
+# with open("mrinal.txt","r") as file:
+#     data=file.read()
+#     print(data)
+
+# with open("mrinal.txt","w") as file:
+#     data=file.write("programming is a cool stuff.")
+#     print(data)
+
+# with open("mrinal.txt","a") as file:
+#     data=file.write("\n i am loving it!!")
+#     print(data)
+    
+    
+    
+    
