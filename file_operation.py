01ab45bd95d484a0e84eea3daaee16d07cb506e2
mrinal1000 <mrinalgrg19@gmail.com>
2024-08-18 09:03:03 +0545

    genertors

--- file_operation.py
+++ file_operation.py
@@ -1,30 +1,10 @@
-#plain text file
-# with open("mrinal.txt","r") as file:
-#     data=file.read()
-#     print(data)
-
-# with open("mrinal.txt","w") as file:
-#     data=file.write("programming is a cool stuff.")
-#     print(data)
-
-# with open("mrinal.txt","a") as file:
-#     data=file.write("\n i am loving it!!")
-#     print(data)
-    
-#   json file
-# import json
-# with open("hero.json","r") as file:
-#     data= json.load(file)
-#     print(data)
-# data={
-#     "school":"apex",
-#     "address":"butwal",
-#     "name":"mrinal"
-# }
-# with open("hero.json","w") as file:
-#     json.dump(data,file,indent=5)
-    
-    
-    
-    
-    
+import csv
+csv_data=[
+    ["name","age","city"]
+    ["ram","36","butwal"]
+    ["hari","15","palpa"]
+]
+with open("villian.csv","w") as file:
+    writer=csv.writer(file)
+    writer.writerows(csv_data)
+    print(writer)
---
+++ genrators.py
@@ -0,0 +1,10 @@
+# def hero():
+#     yield 1
+#     yield 2
+#     yield 3
+
+# gen=hero()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))  
+
