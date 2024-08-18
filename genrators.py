982f2cac46d2d2157969391ac853afc8af6b9eab
mrinal1000 <mrinalgrg19@gmail.com>
2024-08-18 11:51:05 +0545

    generators

--- genrators.py
+++ genrators.py
@@ -8,3 +8,17 @@
 # print(next(gen))
 # print(next(gen))  
 
+# def abc(x,y):
+#     while True:
+#         yield x
+#         x,y=y,x+y
+        
+# okay=abc(30,40)
+# for _ in range (7):
+#     print(next(okay))    
+
+        
+    
+
+
+                   
